54d7f83589ce9785cfb5988adc235cb4c8a2e6f9
Francisco <Franc-900@hotmail.com>
2023-03-02 10:57:49 -0600

    deleted for me

--- types/google-publisher-tag/google-publisher-tag-tests.ts
+++
@@ -1,3 +0,0 @@
-// Tests for Google Publisher Tag 1.20230206
-// Synced from: https://github.com/googleads/google-publisher-tag-types/commit/6814679d5138d8af539cfdf98898fe10640af82b
-googletag.cmd;
--- types/google.maps/google.maps-tests.ts
+++
@@ -1,3 +0,0 @@
-// No tests required for generated types
-// Synced from: https://github.com/googlemaps/js-types/commit/7eda947510b366d49c7024a395ac27ae5bd7b482
-google.maps.Map;
